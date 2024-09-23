0023c2ee3438104c2f21bae85dcc41877c091ad7
Ankit Kumar Singh <ankit210716@gmail.com>
2024-09-23 22:37:17 +0530

    "made Add to Cart button interactive"

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,29 +1,3 @@
-const products = [{
-  image:'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating: {
-    stars:4.5,
-    count: 87
-  },
-  priceCents:1090 // saving money as cents,since JS has issues solving float arithematic
-},{
-  image:'images/products/intermediate-composite-basketball.jpg',
-  name: 'Intermediate Size Basketball',
-  rating: {
-    stars: 4,
-    count: 127
-  },
-  priceCents:2095 // saving money as cents,since JS has issues solving float arithematic
-},{
-  image:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating: {
-    stars: 4.5,
-    count: 56
-  },
-  priceCents:799 // saving money as cents,since JS has issues solving float arithematic
-}];
-
 let productsHTML = '';
 
 products.forEach((product) => {
@@ -72,10 +46,42 @@ products.forEach((product) => {
               Added
             </div>
 
-            <button class="add-to-cart-button button-primary">
+            <button class="add-to-cart-button button-primary js-add-to-cart"
+            data-product-Id="${product.id}">
               Add to Cart
             </button>
           </div>
   `
 })
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart')
+// using product-id and not name because more than 1 product can have same name ...but can be from different brands
+.forEach((addToCartButton) => {
+  addToCartButton.addEventListener('click',() => {
+    const productId = addToCartButton.dataset.productId;
+
+    let matchingItem;
+
+    cart.forEach((item) => {
+      if(productId === item.productId){
+        matchingItem = item;
+      }
+    });
+
+    if(matchingItem){
+      matchingItem.quantity++;
+    }else{
+      cart.push({
+        productId:productId,
+        quantity: 1
+      });
+    }    
+
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    })
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+  })
+})
