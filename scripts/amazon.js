315d0c299d5a03e40b9d2920d7b8f3216edd1f9d
Ankit Kumar Singh <ankit210716@gmail.com>
2024-09-24 19:11:54 +0530

    "changed 'added' and 'quantity' feature and also changed script file to a module"

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,3 +1,7 @@
+// to generate HTML for the products
+import { cart } from "../data/cart.js";
+import { products } from "../data/products.js";
+
 let productsHTML = '';
 
 products.forEach((product) => {
@@ -25,7 +29,7 @@ products.forEach((product) => {
             </div>
 
             <div class="product-quantity-container">
-              <select>
+              <select class="js-quantity-selector-${product.id}">
                 <option selected value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -41,7 +45,7 @@ products.forEach((product) => {
 
             <div class="product-spacer"></div>
 
-            <div class="added-to-cart">
+            <div class="added-to-cart js-added-to-cart-${product.id}">
               <img src="images/icons/checkmark.png">
               Added
             </div>
@@ -55,6 +59,9 @@ products.forEach((product) => {
 })
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
+let timeoutID; // a global scope variable to store setTimeout() id which is used to restore the timer for disappearing of the message
+
+// making Add to cart button responsive
 document.querySelectorAll('.js-add-to-cart')
 // using product-id and not name because more than 1 product can have same name ...but can be from different brands
 .forEach((addToCartButton) => {
@@ -70,18 +77,27 @@ document.querySelectorAll('.js-add-to-cart')
     });
 
     if(matchingItem){
-      matchingItem.quantity++;
+      matchingItem.quantity += Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
     }else{
       cart.push({
         productId:productId,
-        quantity: 1
+        quantity: Number(document.querySelector(`.js-quantity-selector-${productId}`).value)
       });
     }    
-
+    console.log(cart);
     let cartQuantity = 0;
     cart.forEach((item) => {
       cartQuantity += item.quantity;
     })
     document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+    // making 'added' visible and then invisible after 2 seconds
+    document.querySelector(`.js-added-to-cart-${productId}`).classList.add('js-added-to-cart-visible');
+
+    // timer reloads for disappearing of added message
+    clearTimeout(timeoutID);
+
+    timeoutID = setTimeout(() => {
+      document.querySelector(`.js-added-to-cart-${productId}`).classList.remove('js-added-to-cart-visible')
+    },2000);
   })
 })
