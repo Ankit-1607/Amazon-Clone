87ddb8bc9ed895db9b362460c244e16cf25b5b1e
Ankit Kumar Singh <ankit210716@gmail.com>
2024-10-20 19:56:43 +0530

    converted from promises to async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,17 +5,34 @@ import { renderpaymentSummary } from "./checkout/paymentSummary.js";
 // import '../data/backend-practice.js';
 import { loadProducts, loadProductsFetch } from "../data/products.js";
 
+async function loadPage() {
+  console.log('load page');
+
+  await loadProductsFetch();
+
+  renderOrderSummary();
+  renderpaymentSummary();
+  
+  return 'value2';
+}
+loadPage().then((value) => {
+  console.log('next step - 2');
+  console.log(value);
+});
+
 // new Promise((resolve) => {
 //   loadProducts(() => { // asynchronous code
 //     resolve('value1'); // we wait for a-sync code to finish and then go to next step using resolve()
 //   });
 // })
 
+/*
 loadProductsFetch().then((value) => {
   // console.log(value);
   renderOrderSummary();
   renderpaymentSummary();
 });
+*/
 
 /*
 loadProducts(() => {
